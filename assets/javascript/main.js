68db5d6a03878825574d37f2a16b895f97660f4c
cjohnston32 <clayjohnston@protonmail.com>
2018-03-29 15:01:20 -0400

    2) Second Commit - Basic API Functionality

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,8 @@
+/**
+ * @returns {string}
+ */
+
+// makeSearch() - makes the search URL for $.ajax() to access
 function makeSearch() {
     // searchURL is an updated variable, starts with designated API URL
     var searchURL = 'https://projects.propublica.org/nonprofits/api/v2/search.json';
@@ -7,7 +12,7 @@ function makeSearch() {
         // More Information: https://projects.propublica.org/nonprofits/api#methods
         // '?q=' : Keyword search string (name of nonprofit)
         searchURL += '?q=' + userInput;
-
+    console.log(searchURL);
     return searchURL;
 }
 
@@ -15,12 +20,12 @@ function makeSearch() {
  * @param {object} npAPI
  */
 
+// newPage() - updates the page
 function newPage(npAPI) {
-    console.log(npAPI);
-    console.log(charity);
+    console.log(npAPI.total_results);
 };
 
-// For Submitting
+// When user submits search
 $(document).ready(function() {
     $('#submit').on('click', function(event) {
         event.preventDefault();
@@ -29,6 +34,7 @@ $(document).ready(function() {
             url: searchURL,
             method: 'GET',
             dataType: 'jsonp'
-        }).then(newPage);
+        }).then(newPage())
+        .catch(function(err) {alert('error:' + err.message);})
     });
 })
