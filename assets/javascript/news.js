d1d41c89d55402ba5474ebb3a6129b61d3b9e031
Mackenzie Florence <mackflo@Mackenzies-MacBook-Pro.local>
2018-04-06 21:09:09 -0400

    working news api

--- assets/javascript/news.js
+++ assets/javascript/news.js
@@ -1,8 +1,11 @@
+var item;
+
 jQuery.ajaxPrefilter(function(options) {
     if (options.crossDomain && jQuery.support.cors) {
         options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
     }
 });
+
 function results(){
     $("#results").show();
     console.log(status);
@@ -20,18 +23,11 @@ function newPage(newsAPI) {
     console.log(newsDesc);
     document.getElementById('articleList').innerHTML = newsDesc;
 };
-function updatePage() {
-    var indexEnd = element + 9;
-    var elementNum = 0; // 0-9
-    for (index; index<=indexEnd; index++) {
-        // --- Indexing Updates ---
-        elementNum = elementNum.toString();
-        var npDiv = 'np' + elementNum; // ID of div for a single non-profit. Range: np0-np9
-        elementNum = parseInt(elementNum) + 1;}
-    }
 $("#submit").on("click", function(){
-    event.preventDefault();
+        event.preventDefault();
+    document.getElementById('articleInput').innerHTML = '';
     results();
+
     var searchTerm = $("#subject").val().trim();
     var url = 'https://newsapi.org/v2/everything?' +
         'q='+ searchTerm +
@@ -46,8 +42,11 @@ $("#submit").on("click", function(){
         .then(function(jsonRes){
             console.log(jsonRes.articles)
             jsonRes.articles.forEach(element => {
-                $('#articleList > tbody').show().append(("<tr><td><a href="+element.url+">"+element.title + "</td></tr><br>"));
+                var url=element.url;
+                function makeAllURLsWork(){(url.replace('/', ''))};
+                $('#articleInput').show().append(("<tr><td><a href="+element.url+">"+element.title + "</td></tr><br>"));
             });
             
         })
+
 })
